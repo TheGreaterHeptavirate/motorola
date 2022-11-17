b2c4583ed2db7a0712669e8f18b9ad80bad99e6e
gucio321 <73652197+gucio321@users.noreply.github.com>
2022-11-17 17:13:39 +0100

    app: add menu bar

--- pkg/app/rendering.go
+++ pkg/app/rendering.go
@@ -3,7 +3,18 @@ package app
 import "github.com/AllenDang/giu"
 
 func (a *App) render() {
-	giu.SingleWindow().Layout(
+	giu.SingleWindowWithMenuBar().Layout(
+		a.menuBar(),
 		giu.Label("Hello, world!"),
+	)
+}
+
+func (a *App) menuBar() *giu.MenuBarWidget {
+	return giu.MenuBar().Layout(
+		giu.Menu("File").Layout(
+			giu.MenuItem("Open"),
+			giu.MenuItem("Save"),
+			giu.MenuItem("Exit"),
+		),
 	)
 }
