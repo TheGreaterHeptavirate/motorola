7ef116e7537147f26f428e41ee4c226e944c9b0b
gucio321 <73652197+gucio321@users.noreply.github.com>
2022-11-17 17:37:25 +0100

    Revert "update copyright notice"
    This reverts commit d5ef0ce2b08df2e162391c0cd3e0a4fa22436808.

--- pkg/app/rendering.go
+++ pkg/app/rendering.go
@@ -1,11 +1,3 @@
-/*
- * Copyright (c) 2022. Copyright 2022 The Greater Heptavirate team (https://github.com/TheGreaterHeptavirate)
- * All Rights Reserved
- *
- * All copies of this software (if not stated otherway) are dedicated
- * ONLY to personal, non-commercial use.
- */
-
 package app
 
 import "github.com/AllenDang/giu"
