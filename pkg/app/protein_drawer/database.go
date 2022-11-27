9ba30af6195a86e90229a299c2da0d6e5558ecae
gucio321 <73652197+gucio321@users.noreply.github.com>
2022-11-27 19:17:24 +0100

    drawer: add nice separators

--- pkg/app/protein_drawer/database.go
+++ pkg/app/protein_drawer/database.go
@@ -38,6 +38,34 @@ var drawingDatabase = map[string]drawCommand{
 		drawLine(DownRight, standardLine).
 		chemicalText("OH", VAlignCenter, HAlignLeft).
 		move(image.Point{}).draw,
+
+	"F": draw().
+		move(image.Pt(0, 70)).
+		chemicalText("H_3_C", VAlignCenter, HAlignLeft).
+		drawLine(UpRight, standardLine).
+		chemicalText("S", VAlignCenter, HAlignLeft).
+		drawLine(DownRight, standardLine).
+		drawLine(UpRight, standardLine).
+		drawLine(DownRight, standardLine).
+		add(
+			draw().
+				drawLine(Down, standardLine).
+				chemicalText("NH_2_", VAlignTop, HAlignCenter).draw,
+		).
+		ignore(ignoreAll).
+		drawLine(UpRight, standardLine).
+		add(
+			draw().
+				drawLine(Up, standardLine).
+				chemicalText("O", VAlignBottom, HAlignCenter).draw,
+		).
+		ignore(ignoreAll).
+		drawLine(DownRight, standardLine).
+		chemicalText("OH", VAlignCenter, HAlignLeft).
+		move(image.Point{}).draw,
+
+	"[STOP]": draw().
+		chemicalText("STOP", VAlignCenter, HAlignLeft).draw,
 }
 
 func DrawingDatabase() map[string]drawCommand {
--- pkg/app/protein_drawer/drawer.go
+++ pkg/app/protein_drawer/drawer.go
@@ -10,8 +10,10 @@ package protein_drawer
 
 import (
 	"github.com/AllenDang/giu"
+	"github.com/TheGreaterHeptavirate/motorola/pkg/core/inputparser/aminoacid"
 	"github.com/TheGreaterHeptavirate/motorola/pkg/core/inputparser/protein"
 	"image"
+	"math"
 )
 
 func DrawProtein(p *protein.Protein) giu.Widget {
@@ -30,6 +32,37 @@ func DrawProtein(p *protein.Protein) giu.Widget {
 			startPos := image.Pt(cursorPos.X, cursorPos.Y)
 			s := cmd(canvas, startPos)
 			giu.Dummy(float32(s.X), float32(s.Y)).Build()
+
+			if a.Sign != aminoacid.StopCodon {
+				cursorPos := giu.GetCursorScreenPos()
+				startPos := image.Pt(cursorPos.X, cursorPos.Y)
+				a := 10
+				lineLen := int(float32(a) * float32(math.Sqrt2))
+				s = draw().
+					move(image.Pt(0, 20)).
+					// draw top bracket
+					drawLine(DownRight, lineLen).
+					drawLine(Right, (s.X/2)-2*a).
+					drawLine(DownRight, lineLen).
+					drawLine(UpRight, lineLen).
+					drawLine(Right, (s.X/2)-2*a).
+					drawLine(UpRight, lineLen).
+					//
+					move(image.Pt(-s.X/2+a/4, 2*a)).
+					drawLine(Down, 50).
+					move(image.Pt(-s.X/2, 2*a)).
+					//
+					drawLine(UpRight, lineLen).
+					drawLine(Right, (s.X/2)-2*a).
+					drawLine(UpRight, lineLen).
+					drawLine(DownRight, lineLen).
+					drawLine(Right, (s.X/2)-2*a).
+					drawLine(DownRight, lineLen).
+					//
+					move(image.Pt(0, 20)).
+					draw(canvas, startPos)
+				giu.Dummy(float32(s.X), float32(s.Y)).Build()
+			}
 		}
 	}),
 	)
