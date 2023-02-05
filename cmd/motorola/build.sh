4f2a8ae58ec7e6258861286c427137d6c165dfc6
gucio321 <73652197+gucio321@users.noreply.github.com>
2023-02-05 17:04:36 +0100

    windows: make it _theorytically possible_ to cross-compile for windows

--- cmd/motorola/build.sh
+++ cmd/motorola/build.sh
@@ -5,8 +5,8 @@ go build -o motorola.bin
 
 echo "build for windows"
 GOOS=windows GOARCH=amd64 \
-        CGO_ENABLED=1 \
-        CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ \
-        go build \
-        -ldflags="-s -w -linkmode external -extldflags -static" \
-        -o motorola.exe .
+CGO_ENABLED=1 \
+CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ \
+go build \
+-ldflags="-s -w -extldflags -static" \
+-o motorola.exe .
