0f3fa856760498ff5eee70251cd0aa7ffb62f3f9
gucio321 <gucio321@protonmail.com>
2023-02-06 12:40:22 +0100

    wip: imporve biopython itegration

--- pkg/python_integration/biopython_integrator.go
+++ pkg/python_integration/biopython_integrator.go
@@ -10,38 +10,98 @@ import "C"
 
 import (
 	"embed"
-	"io/ioutil"
+	"fmt"
+	"github.com/TheGreaterHeptavirate/motorola/internal/logger"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 //go:embed biopython
 var stuff embed.FS
 
-func init() {
-	Initialize()
-	defer Finalize()
+func InitializeBiopython() (finisher func(), err error) {
+	sysModule, err := OpenPyModule("sys")
+	if err != nil {
+		return nil, fmt.Errorf("error opening python's sys module: %w", err)
+	}
 
-	sysModule, _ := OpenPyModule("sys")
+	logger.Debug("python3: import sys")
+
 	sysModuleDict := C.PyModule_GetDict(sysModule.toC())
+
 	sysPath := C.PyDict_GetItemString(sysModuleDict, C.CString("path"))
 
-	// Add the embedded Python files to sys.path
-	for _, name := range pythonFiles {
-		fileContent, err := stuff.ReadFile(name)
-		if err != nil {
-			panic(err)
-		}
-		file, err := ioutil.TempFile("", name)
-		if err != nil {
-			panic(err)
-		}
-		defer os.Remove(file.Name())
-		_, err = file.Write(fileContent)
-		if err != nil {
-			panic(err)
+	path, err := os.MkdirTemp("", "motorola*-biopython-data")
+	if err != nil {
+		return nil, fmt.Errorf("error creating temporary directory: %w", err)
+	}
+
+	C.PyObject_CallMethodOneArg(sysPath, C.PyUnicode_FromString(C.CString("append")), C.PyUnicode_FromString(C.CString(path)))
+
+	logger.Debugf("tempdir created: %s", path)
+
+	err = loadDir(path, ".", sysPath)
+	if err != nil {
+		return nil, fmt.Errorf("error loading content of directory: %w", err)
+	}
+
+	return nil, nil
+}
+
+func loadDir(base, dirname string, sysPath *C.PyObject) error {
+	files, err := stuff.ReadDir(dirname)
+	if err != nil {
+		return fmt.Errorf("reading directory %s: %w", dirname, err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			dir := joinPath(dirname, file.Name())
+			dirpath := joinPath(base, dirname)
+
+			err = os.Mkdir(joinPath(base, file.Name()), 0o644)
+			if err != nil {
+				return fmt.Errorf("unable to create dir %s: %w", dirpath, err)
+			}
+
+			base = joinPath(base, file.Name())
+
+			logger.Debugf("adding %s to python path", base)
+			//s := C.CString(base)
+			//defer C.free(unsafe.Pointer(s))
+			//C.PyObject_CallMethodOneArg(sysPath, C.PyUnicode_FromString(C.CString("append")), C.PyUnicode_FromString(s))
+
+			err := loadDir(base, dir, sysPath)
+			if err != nil {
+				return err
+			}
+
+			continue
 		}
 
-		C.PyObjectCallMethod(sysPath, C.CString("append"), C.CString(filepath.Dir(file.Name())))
+		filename := joinPath(dirname, file.Name())
+
+		fileData, err := stuff.ReadFile(filename)
+		if err != nil {
+			return fmt.Errorf("reading file %s: %w", filename, err)
+		}
+
+		if filepath.Ext(file.Name()) != ".py" {
+			logger.Debug("file %s has is not a ptyhon file", filename)
+
+			continue
+		}
+
+		err = os.WriteFile(joinPath(base, file.Name()), fileData, 0o644)
+		if err != nil {
+			return fmt.Errorf("error writing to file: %w", err)
+		}
 	}
+
+	return nil
+}
+
+func joinPath(path ...string) string {
+	return strings.ReplaceAll(filepath.Join(path...), "\\", "/")
 }
