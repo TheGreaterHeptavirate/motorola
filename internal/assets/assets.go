d18cb4418a2c4e2ad4e8c5257dbdb8dfd40ab58b
Garnn <bawalilko@gmail.com>
2023-02-16 20:04:00 +0100

    grammar fixes in assets.go

--- internal/assets/assets.go
+++ internal/assets/assets.go
@@ -1,12 +1,12 @@
 /*
- * Copyright (c) 2022 The Greater Heptavirate team (https://github.com/TheGreaterHeptavirate)
+ * Copyright (c) 2023 The Greater Heptavirate team (https://github.com/TheGreaterHeptavirate)
  * All Rights Reserved
  *
- * All copies of this software (if not stated otherway) are dedicated
+ * All copies of this software (if not stated otherwise) are dedicated
  * ONLY to personal, non-commercial use.
  */
 
-// Package assets is intended to contain only the logic
+// Package assets are intended to only contain the logic
 // responsible for calling go:embed on required project's files
 // and storing them in public variables
 //
@@ -17,13 +17,13 @@
 package assets
 
 import (
-	// there is  "_" - black-hole variable so that embed package cannot be used by reference
+	// there is a "_" - black-hole variable so that embed package cannot be used by reference
 	// but it is required to use go:embed directive
 	// for more details about go:embed and embed package, search for "embed go reference" in google.
 	_ "embed"
 )
 
-// AppCSS represents a CSS stylesheet for giu app.
+// AppCSS represents a CSS stylesheet for the giu app.
 var (
 	//go:embed stylesheet/app.css
 	AppCSS []byte
