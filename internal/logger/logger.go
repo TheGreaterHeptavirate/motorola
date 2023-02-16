a01ab9fcc52dfc78d53091d2a69d96a31da705eb
Garnn <bawalilko@gmail.com>
2023-02-16 20:07:29 +0100

    Added commas to logger.go and updated copyright

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022. The Greater Heptavirate: programming lodge (https://github.com/TheGreaterHeptavirate). All Rights Reserved.
+ * Copyright (c) 2023. The Greater Heptavirate: programming lodge (https://github.com/TheGreaterHeptavirate). All Rights Reserved.
  *
  * All copies of this software (if not stated otherwise) are dedicated ONLY to personal, non-commercial use.
  */
@@ -10,19 +10,19 @@ package logger
 import "github.com/kpango/glg"
 
 func init() {
-	// for glg, need to change caller's lenght in order to report real caller path
+	// for glg, need to change caller's length in order to report real caller path
 	// instead of path to this file.
 	glg.Get().SetCallerDepth(3)
 }
 
-// Info logs a message at level Info on the standard logger.
+// Info logs a message at level "Info" on the standard logger.
 func Info(args ...interface{}) {
 	if err := glg.Info(args...); err != nil {
 		panic(err)
 	}
 }
 
-// Infof logs a message at level Info on the standard logger.
+// Infof logs a message at level "Info" on the standard logger.
 // It uses fmt.Sprintf to format the message.
 func Infof(format string, args ...interface{}) {
 	if err := glg.Infof(format, args...); err != nil {
@@ -30,14 +30,14 @@ func Infof(format string, args ...interface{}) {
 	}
 }
 
-// Debug logs a message at level Debug on the standard logger.
+// Debug logs a message at level "Debug" on the standard logger.
 func Debug(args ...interface{}) {
 	if err := glg.Debug(args...); err != nil {
 		panic(err)
 	}
 }
 
-// Debugf logs a message at level Debug on the standard logger.
+// Debugf logs a message at level "Debug" on the standard logger.
 // It uses fmt.Sprintf to format the message.
 func Debugf(format string, args ...interface{}) {
 	if err := glg.Debugf(format, args...); err != nil {
@@ -45,14 +45,14 @@ func Debugf(format string, args ...interface{}) {
 	}
 }
 
-// Warn logs a message at level Warn on the standard logger.
+// Warn logs a message at level "Warn" on the standard logger.
 func Warn(args ...interface{}) {
 	if err := glg.Warn(args...); err != nil {
 		panic(err)
 	}
 }
 
-// Warnf logs a message at level Warn on the standard logger.
+// Warnf logs a message at level "Warn" on the standard logger.
 // It uses fmt.Sprintf to format the message.
 func Warnf(format string, args ...interface{}) {
 	if err := glg.Warnf(format, args...); err != nil {
@@ -60,14 +60,14 @@ func Warnf(format string, args ...interface{}) {
 	}
 }
 
-// Error logs a message at level Error on the standard logger.
+// Error logs a message at level "Error" on the standard logger.
 func Error(args ...interface{}) {
 	if err := glg.Error(args...); err != nil {
 		panic(err)
 	}
 }
 
-// Errorf logs a message at level Error on the standard logger.
+// Errorf logs a message at level "Error" on the standard logger.
 // It uses fmt.Sprintf to format the message.
 func Errorf(format string, args ...interface{}) {
 	if err := glg.Errorf(format, args...); err != nil {
@@ -75,18 +75,18 @@ func Errorf(format string, args ...interface{}) {
 	}
 }
 
-// Fatal logs a message at level Fatal on the standard logger.
+// Fatal logs a message at level "Fatal" on the standard logger.
 func Fatal(args ...interface{}) {
 	glg.Fatal(args...)
 }
 
-// Fatalf logs a message at level Fatal on the standard logger.
+// Fatalf logs a message at level "Fatal" on the standard logger.
 // It uses fmt.Sprintf to format the message.
 func Fatalf(format string, args ...interface{}) {
 	glg.Fatalf(format, args...)
 }
 
-// Success logs a message at level Info on the standard logger.
+// Success logs a message at level "Info" on the standard logger.
 // This announces a successful operation.
 func Success(args ...interface{}) {
 	if err := glg.Success(args...); err != nil {
@@ -94,7 +94,7 @@ func Success(args ...interface{}) {
 	}
 }
 
-// Successf logs a message at level Info on the standard logger.
+// Successf logs a message at level "Info" on the standard logger.
 // It uses fmt.Sprintf to format the message.
 func Successf(format string, args ...interface{}) {
 	if err := glg.Successf(format, args...); err != nil {
