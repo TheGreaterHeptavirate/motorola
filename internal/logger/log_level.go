bfccab3fdd0e62b7b66b8c307c5b2eaec877b2f0
Garnn <bawalilko@gmail.com>
2023-02-16 20:05:10 +0100

    Updated copyright in log_level.go

--- internal/logger/log_level.go
+++ internal/logger/log_level.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022. The Greater Heptavirate: programming lodge (https://github.com/TheGreaterHeptavirate). All Rights Reserved.
+ * Copyright (c) 2023. The Greater Heptavirate: programming lodge (https://github.com/TheGreaterHeptavirate). All Rights Reserved.
  *
  * All copies of this software (if not stated otherwise) are dedicated ONLY to personal, non-commercial use.
  */
