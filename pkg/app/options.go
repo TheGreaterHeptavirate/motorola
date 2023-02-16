810969ddcfa4529ec99d35ea509e1a7689c319e2
Garnn <bawalilko@gmail.com>
2023-02-16 20:24:44 +0100

    fixed copyright, again, i hope this is the last one or i'll just find and replace

--- pkg/app/options.go
+++ pkg/app/options.go
@@ -2,7 +2,7 @@
  * Copyright (c) 2023 The Greater Heptavirate team (https://github.com/TheGreaterHeptavirate)
  * All Rights Reserved
  *
- * All copies of this software (if not stated otherway) are dedicated
+ * All copies of this software (if not stated otherwise) are dedicated
  * ONLY to personal, non-commercial use.
  */
 
