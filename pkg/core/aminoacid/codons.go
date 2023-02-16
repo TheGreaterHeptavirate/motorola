bf71bbadb373c340807860a23920f62fb0fbd02b
Garnn <bawalilko@gmail.com>
2023-02-16 20:30:16 +0100

    Ok somehow this copyright message keeps avoiding getting replaced, I hope this is the last time i check

--- pkg/core/aminoacid/codons.go
+++ pkg/core/aminoacid/codons.go
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2022. The Greater Heptavirate team (https://github.com/TheGreaterHeptavirate)
+ * Copyright (c) 2023 The Greater Heptavirate team (https://github.com/TheGreaterHeptavirate)
  * All Rights Reserved
  *
- * All copies of this software (if not stated otherway) are dedicated
+ * All copies of this software (if not stated otherwise) are dedicated
  * ONLY to personal, non-commercial use.
  */
 
